93067837ddd81074e3e48613575ce238dee7ff7b
slavon <vaceslavmartynov325@gmail.com>
2022-03-20 22:16:11 +0300

    Revert "Lesson_1"
    This reverts commit e00cfda3647b9fdbba4742099f3169e81774bb87.

--- Lesson_2/fibonacci_array.rb
+++
@@ -1,7 +0,0 @@
-  #  Заполнить массив числами фибоначчи до 100
-  fibonacci_array = [0,1]
-  loop do value = fibonacci_array[-1] + fibonacci_array[-2]
-     break if value > 100
-     fibonacci_array << value
-  end
-  puts fibonacci_array
